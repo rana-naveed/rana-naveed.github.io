9ef2f9357484821d9e538efcbe335591ee3d9a7e
rana-naveed <muhammadnaveed944@gmail.com>
2017-05-17 14:20:37 +0500

    index.php

---
+++ index.php
@@ -0,0 +1,705 @@
+ <!DOCTYPE html>
+<head>
+  <title>Blood Bank</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link href="style.css" type="text/css" rel="stylesheet"/>
+</head>
+<body>
+<div class="navbar">
+	<div class="col-md-6">
+		<p style="float:right;  margin-right:20px;margin-top:40px; font-weight:Bold; font-family:Tahoma, Geneva, sans-serif;">Save Life All About Cause</p>
+		<img src="images/img3.jpg" style="height:100px; float:right;">
+	</div>
+	<div class="col-md-6">
+		<?php  include('include/navbar1.php'); ?>
+	</div>
+</div>
+
+<div class="header" style="background:#2e4053; height:100%; margin:0; padding:0;">
+	
+	
+	<div class="navbar" style="background:url(images/nav.png);">
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navHeaderCollapse" style="background:silver;">
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<div class="collapse navbar-collapse navHeaderCollapse" >
+						<ul class="nav navbar-nav navbar-left">
+							<li><a href="index.php">
+							<span style=" ">Home</span>
+							  </a></li>
+							<li><a href="#">
+								<span style=" ">Induction</span>
+							</a></li>
+							<li><a href="#">
+							<span style=" ">ASRC</span>
+							</a></li>
+							<li><a href="#">
+							<span style="  ">Feedback</span>
+							</a></li>
+							<li><a href="#">
+							<span style=" ">FAQ</span>
+							</a></li>
+						</ul>
+					</div>
+	</div>
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+					<div class="carousel slide" id="carousel-438275">
+						<ol class="carousel-indicators">
+							<li data-slide-to="0" data-target="#carousel-438275">
+							</li>
+							<li data-slide-to="1" data-target="#carousel-438275" class="active">
+							</li>
+							<li data-slide-to="2" data-target="#carousel-438275">
+							</li>
+						</ol>
+						<div class="carousel-inner">
+							<div class="item">
+								<img alt="Carousel Bootstrap First" src="images/hdr1.jpg" style="height:350px; width:100%;">
+							</div>
+							<div class="item active">
+								<img alt="Carousel Bootstrap Second" src="images/hdr2.jpg" style="height:350px; width:100%;">
+							</div>
+							<div class="item">
+								<img alt="Carousel Bootstrap Third" src="images/hdr3.jpg" style="height:350px; width:100%;">
+							</div>
+						</div> <a class="left carousel-control" href="#carousel-438275" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#carousel-438275" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
+					</div>
+<center><p style="color:white; font-size:30px;">4 Children With Love</p></center>
+</div>
+
+	<div class="col-md-4"> 
+	
+	<span style="color:red; float:left; font-family:Tahoma, Geneva, sans-serif; margin-top:25px; margin-left:20px;">&nbsp;&nbsp;
+		<img src="images/th.jpg" width="40%" height="60%" style="float:left;border:solid 1px red;margin-left:40px;">
+		<b>Adopt A Patient</b><br>&nbsp;&nbsp;
+		To facilitate patients of 
+		&nbsp;&nbsp;&nbsp;Thalassemia for treatment.<br>
+		&nbsp;&nbsp;
+		To facilitate patients of 
+		&nbsp;&nbsp;&nbsp;Thalassemia for treatment.<br>
+		<br><br><br>
+		<span style="color:red; float:left; font-family:Tahoma, Geneva, sans-serif; margin-top:25px; margin-left:20px;">&nbsp;&nbsp;
+		<img src="images/dr.jpg" width="40%" height="60%" style="float:left;border:solid 1px red;margin-left:20px;">
+		<b>Doctor Can Help!</b><br>&nbsp;&nbsp;
+		Easiest way of 
+		&nbsp;&nbsp;&nbsp;getting Free 
+		&nbsp;&nbsp;&nbsp;Online<br>
+		&nbsp;&nbsp;
+		medical advice from Doctors.
+		&nbsp;&nbsp;&nbsp;Thalassemia for treatment.<br>
+		<br><br><br>
+		<span style="color:red; float:left; font-family:Tahoma, Geneva, sans-serif; margin-top:25px; margin-left:20px;">&nbsp;&nbsp;
+		<img src="images/hp.jpg" width="40%" height="60%" style="float:left; border:solid 1px red;">
+		<b>How Can Help?</b><br>&nbsp;&nbsp;
+		Many things you&nbsp;&nbsp;
+		can do to &nbsp;&nbsp;make change by 
+		&nbsp;&nbsp;helping!<br>
+		<br><br><br>
+	
+	</div>
+	<div class="col-md-4">
+	<center><h1><span style="font-family:Tahoma, Geneva, sans-serif; color:red;">HelpLine - Lahore<span></h1></center>
+	<table class="table table-striped table-hover" style="background:red;">
+
+      <tbody><tr class="tblListHdr">
+
+        <td>Sr.</td>
+
+		<!--<td>Type</td>-->
+
+        <td>Hospitals - Lahore</td>
+
+		<td>Contact No</td>
+
+
+
+      </tr> <tr class="mouseover">
+
+				<td>1&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Ganga Ram Hospital&nbsp;</td>
+
+				<td>042-36362152&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>2&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Lady Willington Hospital&nbsp;</td>
+
+				<td>042-37659001-04&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>3&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Mayo Hospital&nbsp;</td>
+
+				<td>042-99211101-09&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>4&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Services Hospital&nbsp;</td>
+
+				<td>042-37588351-59&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>5&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Aadil Hospital&nbsp;</td>
+
+				<td>042-36667272-75&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>6&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Aadil Hospital&nbsp;</td>
+
+				<td>042-36672261-65&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>7&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Al-Fadal Hospital&nbsp;</td>
+
+				<td>042-37281913&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>8&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Al-Hafeez Hospital&nbsp;</td>
+
+				<td>042-37276148&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>9&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Al-Hafeez Hospital&nbsp;</td>
+
+				<td>042-37284580&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>10&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Al-Shafi Hospital&nbsp;</td>
+
+				<td>042-35415918&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>11&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Al-Shaikh Welfare Hospital&nbsp;</td>
+
+				<td>042-37930443&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>12&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Allama Iqbal Medical College&nbsp;</td>
+
+				<td>042-35160107&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>13&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Amna Jabbar Hpspital&nbsp;</td>
+
+				<td>042-36676915&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>14&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Anmol Hospital&nbsp;</td>
+
+				<td>042-99231064-68&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>15&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Anwar Riaz Qadeer Diabetes Center&nbsp;</td>
+
+				<td>042-37564276&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>16&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Badar Medical Complex&nbsp;</td>
+
+				<td>042-37356764&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>17&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Bajwah Hospital&nbsp;</td>
+
+				<td>042-37925589&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>18&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Bajwah Hospital&nbsp;</td>
+
+				<td>042-37925582&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>19&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Baloch Hospital&nbsp;</td>
+
+				<td>042-36824301-02&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>20&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Barkat Hospital&nbsp;</td>
+
+				<td>042-35414967&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>21&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Bashir Medical Center&nbsp;</td>
+
+				<td>042-37227341&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>22&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Bismillah Hospital&nbsp;</td>
+
+				<td>042-37352832&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>23&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Bone and Joint Hospital&nbsp;</td>
+
+				<td>042-37579997&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>24&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Cardex Hospital&nbsp;</td>
+
+				<td>042-37573381-83&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>25&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Carins Railway Hospital&nbsp;</td>
+
+				<td>042-39201720-21&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>26&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>China Hospital&nbsp;</td>
+
+				<td>042-37721433&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>27&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Civil Hospital&nbsp;</td>
+
+				<td>042-99211100-09&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>28&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Cosmetic Plastic Surgeon&nbsp;</td>
+
+				<td>042-36367781&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>29&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Data Darbar Hospital&nbsp;</td>
+
+				<td>042-37248603&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>30&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Defence Hospital&nbsp;</td>
+
+				<td>042-35722728&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>31&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>PSYCHIATRIC Hospital&nbsp;</td>
+
+				<td>042-37589167&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>32&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Ghulab Devi&nbsp;</td>
+
+				<td>042-35831758-61&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>33&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Janki Devi&nbsp;</td>
+
+				<td>042-36364195&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>34&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>General Hospital&nbsp;</td>
+
+				<td>042-5810891-98&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>35&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Shalimar Hospital&nbsp;</td>
+
+				<td>042-36817857-9&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>36&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Fatima Memorial Hospital&nbsp;</td>
+
+				<td>042-37580681-83&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>37&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Lady Atichison&nbsp;</td>
+
+				<td>042-37227487&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>38&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Sheikh Zayed Hospital&nbsp;</td>
+
+				<td>042-35865731-36&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>39&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>U.C.H Hospital&nbsp;</td>
+
+				<td>042-35763573-75&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>40&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Jinnah Hospital&nbsp;</td>
+
+				<td>042-35162478&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>41&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Surgimed Hospital, 1 Zafar Ali Road&nbsp;</td>
+
+				<td>042-35714411-18&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>42&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Civil Hospital (Casualties)&nbsp;</td>
+
+				<td>042-37310130-9&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>43&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Queens Railway&nbsp;</td>
+
+				<td>042-99201721&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>44&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Data Darbar Hospital&nbsp;</td>
+
+				<td>042-37248603&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>45&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Dental Hospital&nbsp;</td>
+
+				<td>042-37659024&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>46&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Itifaq hospital&nbsp;</td>
+
+				<td>042-35881981&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>47&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Mental Hospital&nbsp;</td>
+
+				<td>042-37589167&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>48&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Mian Mohammad Munshi Hospital&nbsp;</td>
+
+				<td>042-37225521&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>49&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Pujab Institute of Cardialogy&nbsp;</td>
+
+				<td>042-37589721&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>50&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Services Hospital&nbsp;</td>
+
+				<td>042-39203425&nbsp;</td>
+
+			  </tr><tr class="mouseover">
+
+				<td>51&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Shuakat Khanam Hospital&nbsp;</td>
+
+				<td>042-35180725&nbsp;</td>
+
+			  </tr><tr class="mouseover" id="bgList">
+
+				<td>52&nbsp;</td>
+
+				<!--<td>Hospitals&nbsp;</td>-->
+
+				<td>Wapda Hospital&nbsp;</td>
+
+				<td>042-37590711&nbsp;</td>
+
+			  </tr><tr>
+
+        <td>&nbsp;</td>
+
+       <!-- <td>&nbsp;</td>-->
+
+		<td>&nbsp;</td>
+
+		<td>&nbsp;</td>
+
+
+
+      </tr>
+
+    </tbody></table> 
+	
+	
+	</div>
+	<div class="col-md-4">
+	
+
+		<span style="color:red; float:left; font-family:Tahoma, Geneva, sans-serif; margin-top:25px; margin-left:20px;">&nbsp;&nbsp;
+		<img src="images/th.jpg" width="40%" height="60%" style="float:left;border:solid 1px red;margin-left:40px;">
+		<b>Adopt A Patient</b><br>&nbsp;&nbsp;
+		To facilitate patients of 
+		&nbsp;&nbsp;&nbsp;Thalassemia for treatment.<br>
+		&nbsp;&nbsp;
+		To facilitate patients of 
+		&nbsp;&nbsp;&nbsp;Thalassemia for treatment.<br>
+		<br><br><br>
+		<span style="color:red; float:left; font-family:Tahoma, Geneva, sans-serif; margin-top:25px; margin-left:20px;">&nbsp;&nbsp;
+		<img src="images/dr.jpg" width="40%" height="60%" style="float:left;border:solid 1px red;margin-left:20px;">
+		<b>Doctor Can Help!</b><br>&nbsp;&nbsp;
+		Easiest way of 
+		&nbsp;&nbsp;&nbsp;getting Free 
+		&nbsp;&nbsp;&nbsp;Online<br>
+		&nbsp;&nbsp;
+		medical advice from Doctors.
+		&nbsp;&nbsp;&nbsp;Thalassemia for treatment.<br>
+		<br><br><br>
+		<span style="color:red; float:left; font-family:Tahoma, Geneva, sans-serif; margin-top:25px; margin-left:20px;">&nbsp;&nbsp;
+		<img src="images/hp.jpg" width="40%" height="60%" style="float:left; border:solid 1px red;">
+		<b>How Can Help?</b><br>&nbsp;&nbsp;
+		Many things you&nbsp;&nbsp;
+		can do to &nbsp;&nbsp;make change by 
+		&nbsp;&nbsp;helping!<br>
+		<br><br><br>
+
+	</div>
+
+</body>
+</html>
