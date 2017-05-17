732690abea00668cd60152c92e26d5d4cb5b1f04
rana-naveed <muhammadnaveed944@gmail.com>
2017-05-17 15:19:16 +0500

    mail.php

--- index.php
+++ index.php
@@ -1,72 +1,104 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>Geocoding service</title>
-    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">
-    <style>
-      /* Always set the map height explicitly to define the size of the div
-       * element that contains the map. */
-      #map {
-        height: 100%;
-      }
-      /* Optional: Makes the sample page fill the window. */
-      html, body {
-        height: 100%;
-        margin: 0;
-        padding: 0;
-      }
-      #floating-panel {
-        position: absolute;
-        top: 10px;
-        left: 25%;
-        z-index: 5;
-        background-color: #fff;
-        padding: 5px;
-        border: 1px solid #999;
-        text-align: center;
-        font-family: 'Roboto','sans-serif';
-        line-height: 30px;
-        padding-left: 10px;
-      }
-    </style>
-  </head>
-  <body>
-    <div id="floating-panel">
-      <input id="address" type="textbox" value="Sydney, NSW">
-      <input id="submit" type="button" value="Geocode">
-    </div>
-    <div id="map"></div>
-    <script>
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 8,
-          center: {lat: -34.397, lng: 150.644}
-        });
-        var geocoder = new google.maps.Geocoder();
+<head>
+<title>Form</title>
+</head>
 
-        document.getElementById('submit').addEventListener('click', function() {
-          geocodeAddress(geocoder, map);
-        });
-      }
+<body>
 
-      function geocodeAddress(geocoder, resultsMap) {
-        var address = document.getElementById('address').value;
-        geocoder.geocode({'address': address}, function(results, status) {
-          if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-              map: resultsMap,
-              position: results[0].geometry.location
-            });
-          } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-          }
-        });
-      }
-    </script>
-    <script async defer
-    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDjn-tCjcQ-tkmmiYp4v6Eg31cCFqBdFlg&callback=initMap">
-    </script>
-  </body>
+<form id='contactus' action='contact.php' enctype="multipart/form-data" method='post'>
+
+<fieldset >
+<legend>Contact us</legend>
+
+
+<div class='container'>
+  
+    <label for='email' >Name*:</label><br/>
+    <input type="text" id="name" name="name" required /><br>
+    <label for='email' >Phone*:</label><br/>
+    <input type="text" id="phone" name="phone" required /><br>
+    <label for='email' >Email*:</label><br/>
+    <input type='text' name='email' id='email' required/><br/>
+ 
+    <label for='message' >Message:</label><br/>
+    <textarea rows="10" cols="50" name='message' id='message'></textarea>
+    <br>
+    <!-- Name of input element determines name in $_FILES array -->
+    Send this file: <input id="file" name="image" type="file" />
+    <input type='submit' name='Submit' value='Submit' />
+</div>
+
+</fieldset>
+</form>
+    </body>
 </html>
+
+
+<?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size = $_FILES['image']['size'];
+      $file_tmp = $_FILES['image']['tmp_name'];
+      $file_type = $_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $expensions= array("jpeg","jpg","png","pdf");
+      
+      if(in_array($file_ext,$expensions)=== false){
+         $errors[]="extension not allowed, please choose a PDF, JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152) {
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true) {
+         move_uploaded_file($file_tmp,"uploads/".$file_name); //The folder where you would like your file to be saved
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+
+// PHPMailer script below
+
+$email = $_REQUEST['email'] ;
+$name = $_REQUEST['name'] ;
+$phone = $_REQUEST['phone'] ;
+$message = $_REQUEST['message'] ;
+require("phpmailer/PHPMailerAutoload.php");
+
+$mail = new PHPMailer();
+
+$mail->IsSMTP();
+
+$mail->Host = "smtp.gmail.com";
+
+$mail->SMTPAuth = true; 
+
+$mail->Username = "yoursmtp@username.com"; // SMTP username
+$mail->Password = "hidden"; // SMTP password
+$mail->addAttachment("uploads/".$file_name);
+$mail->From = $email;
+$mail->SMTPSecure = 'tls'; 
+$mail->Port = 587; //SMTP port
+$mail->addAddress("your@email.com", "your name");
+$mail->Subject = "You have an email from a website visitor!";
+$mail->Body ="
+Name: $name<br>
+Email: $email<br>
+Telephone: $phone<br><br><br>
+Comments: $message";
+$mail->AltBody = $message;
+
+if(!$mail->Send())
+{
+echo "Message could not be sent. <p>";
+echo "Mailer Error: " . $mail->ErrorInfo;
+exit;
+}
+
+echo "<script>alert('Message has been sent')</script>";
+?>
